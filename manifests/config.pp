d0233617ec04d48e87099b4952b69d755b1c819c
Lueder Thaele <l.thaele@metaways.de>
2017-04-03 16:39:11 +0200

    fix

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,13 +2,17 @@
 #
 # Configuration
 #
-class kibana4::config {
+class kibana4::config 
+(
+  $config_path = '/opt/kibana/config/kibana.yml',
+)
+{
 
   if $kibana4::config {
 
     file { 'kibana-config-file':
       ensure  => file,
-      path    => '/opt/kibana/config/kibana.yml',
+      path    => "$config_path",
       owner   => 'kibana',
       group   => 'kibana',
       mode    => '0755',
